1ee6b37135f9f8bbf9ee477463ac68960ea11a4a
Doni Alston <donialston46@gmail.com>
2025-09-05 17:37:17 +0530

    Add PostgreSQL support

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -75,11 +75,9 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 # Database config
 if os.environ.get("RENDER") == "True":
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
         }
-    }
+    
 else:
     DATABASES = {
         'default': {
